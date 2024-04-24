96cf424d2720eafa40fc0db891009d904952a108
Kevin Malakoff <kmalakoff@gmail.com>
2024-04-24 12:18:57 -0700

    Upgrade modules

--- test/spec/index.test.js
+++ test/spec/index.test.js
@@ -1,14 +1,14 @@
-var assert = require('assert');
-var ts = require('../..');
+const assert = require('assert');
+const ts = require('ts-constants');
 
-describe('ts-constants', function() {
-  it('has ScriptTarget', function(){
-    assert.ok(!!ts.ScriptTarget)
-  })
-  it('has ModuleKind', function(){
-    assert.ok(!!ts.ModuleKind)    
-  })
-  it('has const JsxEmit', function(){
-    assert.ok(!!ts.JsxEmit)
-  })
-})
+describe('ts-constants', () => {
+  it('has ScriptTarget', () => {
+    assert.ok(!!ts.ScriptTarget);
+  });
+  it('has ModuleKind', () => {
+    assert.ok(!!ts.ModuleKind);
+  });
+  it('has const JsxEmit', () => {
+    assert.ok(!!ts.JsxEmit);
+  });
+});
